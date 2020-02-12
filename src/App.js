f2683a28c6ecc11785e66eb5c4e34f35d068845f
unknown <svetlanakoshman1@gmail.com>
2020-02-12 02:00:46 +0100

    navigation

--- src/App.js
+++ src/App.js
@@ -2,16 +2,27 @@ import React, {Component} from 'react';
 import Navbar from './components/Navbar/Navbar';
 import "./App.scss";
 import TourList from './components/TourList';
+import About from './About';
+import Home from './Home';
+import{BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 
 
 import './App.scss';
 
 class App extends Component {
   render() {
-    return (<main>
-      <Navbar />
+    return (
+    <Router>
+      <main>
+        <Navbar />
+        <Switch>
+        <Route path="/" exact component={Home}/>
+        <Route path="/about" component={About}/>
+        </Switch>
       <TourList />
     </main>
+    </Router>
+    
     )
   }
 }
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,23 +1,31 @@
 import React from 'react';
 import './Navbar.scss';
 import logo from '../../logo.png';
+import {Link}from'react-router-dom'
 
 export default function Navbar(){
 
     return(
         
     <nav className="navbar">
+        
         <img src={logo} aæt='city tours logo' />
         <ul className="nav-links">
+            <Link to='/'>
             <li>
-                <a href="/" className="nav-link">Home</a>
+                <a href="/" className="nav-link active">Home</a>
             </li>
+            </Link>
+            <Link to='/about'>
             <li>
                 <a href="/" className="nav-link">About</a>
             </li>
+            </Link>
+            <Link to='/Tour'>
             <li>
-                <a href="/" className="nav-link active">Tours</a>
+                <a href="/" className="nav-link">Tours</a>
             </li>
+            </Link>
         </ul>
 
     </nav>
